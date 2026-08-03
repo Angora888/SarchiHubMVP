8c2b6bc56bdb313df6f9fc51a43b7bb881e37a83
Luis Antony Alfaro Salas <angora88@gmail.com>
2026-08-03 13:54:55 -0600

    Enviroment variable change en Programs

--- RestaurantHub.Api/Program.cs
+++ RestaurantHub.Api/Program.cs
@@ -71,5 +71,5 @@ app.UseAuthentication();
 app.UseStaticFiles();
 app.UseAuthorization();
 app.MapControllers();
-app.Urls.Add("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT"));
+//app.Urls.Add("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT"));
 app.Run();
